c2f4250bc3cc6abf9975aaa0b9b39baa3705e4b0
Pedro014camp <pedrorm_014@hotmail.com>
2024-08-26 22:01:42 -0600

    Adicion de la funcionalidad para desencriptar un texto

--- index.js
+++ index.js
@@ -25,3 +25,30 @@ if(texto.length != 0){
 }
 
 }
+
+function desencriptar(){
+let texto = document.getElementById ("texto").value;
+let tituloMensaje= document.getElementById("titulo-mensaje");
+let parrafo= document.getElementById("parrafo");
+let muñeco = document.getElementById("muñeco");
+
+    let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+
+if(texto.length != 0){
+    document.getElementById ("texto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto desencriptado con exito";
+    parrafo.textContent = "";
+    muñeco.src= "./imagenes/desencriptado.jpg";
+} else{
+    muñeco.src= "./imagenes/muñeco.png";
+    tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    alert ("Debes ingresar algún texto");
+}
+}
+
